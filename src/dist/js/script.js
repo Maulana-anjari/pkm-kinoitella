4f4790866ef8c13428383dcd04ec5a8b8f50fd32
Maulana-anjari <maulana17anjari@gmail.com>
2024-06-22 21:32:52 +0700

    fix bug

--- src/dist/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamberger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu")
-hamberger.addEventListener('click', () => {
-    hamberger.classList.toggle('is-active');
-    menu.classList.toggle("menu-active");
-})
